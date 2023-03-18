906d0a097a8a3d6948281a67e33e71b40bf802ce
nilesh3535 <kumbharnilesh4@gmail.com>
2023-03-18 09:40:59 +0530

    1st commit project upload

---
+++ src/constants/data.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    vurl: 'https://vs.socialverseapp.com/hackinet_11bbb8bc7d44e09cbc6b3b2086b1aee54dbc44d5541/hackinet_11bbb8bc7d44e09cbc6b3b2086b1aee54dbc44d5541_flic.mp4',
+    vthumb:
+      'https://vs.socialverseapp.com/hackinet_11bbb8bc7d44e09cbc6b3b2086b1aee54dbc44d5541/hackinet_11bbb8bc7d44e09cbc6b3b2086b1aee54dbc44d5541.0000005.jpg',
+    uid: 'hackinet',
+    upic: 'https://cdn-assets.socialverseapp.com/profile/flic-avatar-e2f9d20ea76db29b03dc4a5b4abb31d4.png',
+  },
+  {
+    vurl: 'https://vs.socialverseapp.com/aftobeezy_164675cacf1a9d7564e167a95ab00535b743f56d268/aftobeezy_164675cacf1a9d7564e167a95ab00535b743f56d268_flic.mp4',
+    vthum:
+      'https://vs.socialverseapp.com/aftobeezy_164675cacf1a9d7564e167a95ab00535b743f56d268/aftobeezy_164675cacf1a9d7564e167a95ab00535b743f56d268.0000005.jpg',
+    uid: 'aftobeezy',
+    upic: 'https://cdn-assets.socialverseapp.com/profile/flic-avatar-a4ba39c9968f262300e3aba011e21932.png',
+  },
+  {
+    vurl: 'https://vs.socialverseapp.com/hackinet_fc5fbe562becddbf95b546d309a71a8c5d304cc5899/hackinet_fc5fbe562becddbf95b546d309a71a8c5d304cc5899_flic.mp4',
+    vthumb:
+      'https://vs.socialverseapp.com/hackinet_fc5fbe562becddbf95b546d309a71a8c5d304cc5899/hackinet_fc5fbe562becddbf95b546d309a71a8c5d304cc5899.0000005.jpg',
+    uid: 'hackinet',
+    upic: 'https://cdn-assets.socialverseapp.com/profile/flic-avatar-e2f9d20ea76db29b03dc4a5b4abb31d4.png',
+  }
+];
